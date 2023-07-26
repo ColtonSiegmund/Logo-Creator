7011cc22728b8b5b2628a989c7aed4f0700d1324
ColtonSiegmund <135175571+ColtonSiegmund@users.noreply.github.com>
2023-07-26 09:53:49 -0500

    added shapes to library folder

---
+++ library/shapes.js
@@ -0,0 +1,19 @@
+
+// SVG Circle
+
+`<svg width="300" height="300">
+  <circle cx="150" cy="150" r="140" stroke-width="4" fill="yellow" />
+<text x="110" y="150" font-family="Verdana" font-size="35" fill="blue">CWS</text>
+</svg>`
+
+// SVG Square
+`<svg width="400" height="300">
+  <rect width="300" height="300" style="fill:rgb(0,0,255);" />
+  <text x="110" y="150" font-family="Verdana" font-size="35" fill="white">CWS</text>
+</svg>`
+
+// SVG Triangle
+`<svg height="210" width="500">
+  <polygon points="170,0 350,250 20,210" style="fill:lime;" />
+<text x="128" y="150" font-family="Verdana" font-size="35" fill="white">CWS</text>
+</svg>`
